ddcfe069434a7a0cfaa39191e3f1f6bce41644d7
Stefany-Silveira <stefany.silveira21@gmail.com>
2022-09-06 20:11:17 -0300

    add @ LocacaoServiceTest

---
+++ src/test/java/br/com/stefany/tu/servicos/LocacaoServiceTest.java
@@ -0,0 +1,32 @@
+package br.com.stefany.tu.servicos;
+
+import br.com.stefany.tu.entidades.Filme;
+import br.com.stefany.tu.entidades.Locacao;
+import br.com.stefany.tu.entidades.Usuario;
+import br.com.stefany.tu.servicos.LocacaoService;
+import br.com.stefany.tu.utils.DataUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+public class LocacaoServiceTest {
+
+    @Test
+    public void teste() {
+
+        //cenario
+        LocacaoService service = new LocacaoService();
+        Usuario usuario = new Usuario("Usuario 1");
+        Filme filme = new Filme("Filme 1", 2, 5.0);
+
+        //ação
+        Locacao locacao = service.alugarFilme(usuario, filme);
+
+        //verificacao
+        Assertions.assertTrue(locacao.getValor() == 5.0);
+        Assertions.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+        Assertions.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(),
+                DataUtils.obterDataComDiferencaDias(1)));
+    }
+}
