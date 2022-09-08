bf000c7353f7a57e2117342265681046f286e8a4
Stefany-Silveira <stefany.silveira21@gmail.com>
2022-09-07 21:08:38 -0300

    add @ Formas de dividir um teste

--- src/test/java/br/com/stefany/tu/servicos/LocacaoServiceTest.java
+++ src/test/java/br/com/stefany/tu/servicos/LocacaoServiceTest.java
@@ -1,5 +1,6 @@
 package br.com.stefany.tu.servicos;
 
+import org.junit.Rule;
 import org.junit.jupiter.api.Test;
 
 import java.util.Date;
@@ -8,17 +9,19 @@ import static br.com.stefany.tu.utils.DataUtils.isMesmaData;
 import static br.com.stefany.tu.utils.DataUtils.obterDataComDiferencaDias;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.not;
-import static org.junit.Assert.assertThat;
 
 import br.com.stefany.tu.entidades.Filme;
 import br.com.stefany.tu.entidades.Locacao;
 import br.com.stefany.tu.entidades.Usuario;
+import org.junit.rules.ErrorCollector;
 
 public class LocacaoServiceTest {
 
+    @Rule
+    public ErrorCollector error = new ErrorCollector();
+
     @Test
-    public void teste() {
+    public void testeLocacao() {
 
         //cenario
         LocacaoService service = new LocacaoService();
@@ -29,9 +32,9 @@ public class LocacaoServiceTest {
         Locacao locacao = service.alugarFilme(usuario, filme);
 
         //verificacao
-        assertThat(locacao.getValor(), is(equalTo(5.0)));
-        assertThat(locacao.getValor(), is(not(6.0)));
-        assertThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
-        assertThat(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1)), is(true));
+        error.checkThat(locacao.getValor(), is(equalTo(6.0)));
+        error.checkThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
+        error.checkThat(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1)),
+                is(false));
     }
 }
