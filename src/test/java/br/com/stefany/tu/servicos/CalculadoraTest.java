da83bdd3c5d8e063acd4ad3f294d46792816b4f2
“Stefany-Silveira” <stefany.silveira21@gmail.com>
2022-09-12 09:27:21 -0300

    add @ Análise de Cobertura

--- src/test/java/br/com/stefany/tu/servicos/CalculadoraTest.java
+++ src/test/java/br/com/stefany/tu/servicos/CalculadoraTest.java
@@ -60,4 +60,14 @@ public class CalculadoraTest {
 
         calc.divisao(a, b);
     }
+
+    @Test
+    public void deveDividir() {
+        String a ="6";
+        String b = "3";
+
+        int resultado = calc.divide(a, b);
+
+        Assert.assertEquals(2, resultado);
+    }
 }
